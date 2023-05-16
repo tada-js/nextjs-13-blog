b1de339706aa4ca5cd2d9e119c7e89227ce9d8ba
tada-js <justfix010@gmail.com>
2023-05-17 01:37:32 +0900

    Style : Header 컴포넌트

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,12 +2,12 @@ import Link from 'next/link';
 
 const Header = () => {
   return (
-    <header>
+    <header className="flex items-center justify-between p-4">
       <Link href="/">
-        <h1>My Blog</h1>
+        <h1 className="text-3xl font-light">My Blog</h1>
       </Link>
 
-      <nav>
+      <nav className="flex gap-4">
         <Link href="/">Home</Link>
         <Link href="/posts">게시물</Link>
         <Link href="/about">소개</Link>
