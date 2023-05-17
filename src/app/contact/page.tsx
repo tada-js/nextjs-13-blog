0a3aaa1dc8bdf98f6ae290cf247be206597fa86a
tada-js <justfix010@gmail.com>
2023-05-17 18:23:55 +0900

    Style : contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,17 +1,39 @@
 import { Metadata } from 'next';
+import { AiFillGithub } from 'react-icons/ai';
 
 export const metadata: Metadata = {
   title: '문의',
   description: '개발자에게 문의',
 };
 
+const LINKS = [{ icon: <AiFillGithub />, url: 'https://github.com/tada-js' }];
+
 const ContactPage = () => {
   return (
-    <section>
-      <h2>연락 정보</h2>
-      <p>justfix010@gmail.com</p>
-      <h2>이메일 보내기</h2>
-    </section>
+    <>
+      <section className="m-8 flex flex-col items-center -bg--sub-color shadow-lg">
+        <h2 className="my-4 text-2xl font-semibold -text--text-brand-3">
+          연락 정보
+        </h2>
+        <article className="flex w-60 items-center justify-between">
+          {LINKS.map((link, index) => (
+            <a
+              key={index}
+              href={link.url}
+              target="_blank"
+              rel="noreferrer"
+              className="ition text-4xl duration-300 hover:text-orange-400 hover:transition"
+            >
+              {link.icon}
+            </a>
+          ))}
+          <p className="text-gray-500">justfix010@gmail.com</p>
+        </article>
+        <h2 className="my-8 text-2xl font-semibold -text--text-brand-3">
+          이메일 보내기
+        </h2>
+      </section>
+    </>
   );
 };
 
