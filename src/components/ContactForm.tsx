dc3c5385fb9b44f312809827e9cf6291587df63e
tada-js <justfix010@gmail.com>
2023-05-18 20:43:28 +0900

    Feat : ContactForm form 추가

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import { ChangeEvent, FormEvent, useState } from 'react';
+
 interface Form {
   from: string;
   subject: string;
@@ -17,9 +19,23 @@ const LABEL_CLASS =
 const INPUT_CLASS = 'pl-1 text-black';
 
 const ContactForm = () => {
+  const [form, setForm] = useState<Form>(DEFAULT_FORM);
+
+  const onChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const { name, value } = e.target;
+    setForm((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const onSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
   return (
     <section className="my-2 w-full max-w-lg text-gray-600">
-      <form className="flex w-full flex-col gap-2 rounded-xl bg-[#C4DFDF] p-4 shadow-md">
+      <form
+        className="flex w-full flex-col gap-2 rounded-xl bg-[#C4DFDF] p-4 shadow-md"
+        onSubmit={onSubmit}
+      >
         <label htmlFor="from" className={LABEL_CLASS}>
           이메일
         </label>
@@ -29,6 +45,7 @@ const ContactForm = () => {
           name="from"
           className={INPUT_CLASS}
           placeholder="example@mail.com"
+          onChange={onChange}
           required
           autoFocus
         />
@@ -41,6 +58,7 @@ const ContactForm = () => {
           name="subject"
           className={INPUT_CLASS}
           placeholder="제목"
+          onChange={onChange}
           required
         />
         <label htmlFor="message" className={LABEL_CLASS}>
@@ -52,6 +70,8 @@ const ContactForm = () => {
           name="message"
           className={INPUT_CLASS}
           placeholder="내용을 입력해 주세요."
+          onChange={onChange}
+          required
         />
         <button className="mx-auto my-2 w-28 rounded-lg -bg--bg-brand-3 p-2 font-semibold text-slate-100 hover:-bg--bg-brand-1">
           제출
