5d7ce7cb0b6d95817731f78b019467e8821b9f84
tada-js <justfix010@gmail.com>
2023-05-20 03:11:43 +0900

    Feat : post slug page, posts 모듈

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,5 +1,27 @@
-const PostPage = () => {
-  return <div>page</div>;
+import { getPostData } from '@/service/posts';
+import Image from 'next/image';
+
+interface Props {
+  params: {
+    slug: string;
+  };
+}
+
+const PostPage = async ({ params }: Props) => {
+  const post = await getPostData(params.slug);
+  const { title, path, next, prev } = post;
+
+  return (
+    <article>
+      <Image
+        src={`/images/posts/${path}.png`}
+        alt={title}
+        width={760}
+        height={420}
+      />
+      <section></section>
+    </article>
+  );
 };
 
 export default PostPage;
--- src/service/posts.ts
+++ src/service/posts.ts
@@ -11,9 +11,32 @@ export interface Post {
   featured: boolean;
 }
 
+export interface PostData extends Post {
+  content: string;
+  next: Post | null;
+  prev: Post | null;
+}
+
 export const getAllPosts = cache(async (): Promise<Post[]> => {
   const filePath = path.join(process.cwd(), 'data', 'posts.json');
   return readFile(filePath, 'utf-8')
     .then<Post[]>(JSON.parse)
     .then((posts) => posts.sort((a, b) => (a.date > b.date ? -1 : 1)));
 });
+
+export const getPostData = async (fileName: string): Promise<PostData> => {
+  const filePath = path.join(process.cwd(), 'data', 'posts', `${fileName}.md`);
+  const posts = await getAllPosts();
+  const post = posts.find((post) => post.path === fileName);
+
+  if (!post) {
+    throw new Error(`${fileName}에 해당하는 포스트를 찾을 수 없습니다.`);
+  }
+
+  const index = posts.indexOf(post);
+  const next = index > 0 ? posts[index - 1] : null;
+  const prev = index < posts.length - 1 ? posts[index + 1] : null;
+
+  const content = await readFile(filePath, 'utf-8');
+  return { ...post, content, next, prev };
+};
