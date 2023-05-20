8fff4a982da7af41ebe90ec88c59fd1f838f165d
tada-js <justfix010@gmail.com>
2023-05-21 02:51:40 +0900

    Style : Carouselposts 컴포넌트

--- src/components/CarouselPosts.tsx
+++ src/components/CarouselPosts.tsx
@@ -6,13 +6,16 @@ import PostCard from './PostCard';
 const CarouselPosts = async () => {
   const posts = await getNonuredePosts();
   return (
-    <section>
-      <div>
-        <h2>다른 게시물</h2>
+    <section className="mx-4 my-16">
+      <div className="flex items-center justify-between my-2">
+        <h2 className="text-2xl font-medium text-left -text--text-brand-3">
+          다른 게시물
+        </h2>
         <Link href="/posts">
-          <h3>더 보기</h3>
+          <h3 className="text-gray-400 hover:text-gray-600">더 보기</h3>
         </Link>
       </div>
+      <div className="mb-4 border-b-4 border-orange-100 w-28"></div>
       <MultiCarousel>
         {posts.map((post) => (
           <PostCard key={post.path} post={post} />
